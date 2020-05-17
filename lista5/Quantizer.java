18eaba334ba9fd030113be1f5f38799fcd895681
malinowska423 <amalinowska423@gmail.com>
2020-05-17 22:27:03 +0200

    lista 5 dziala

--- lista5/Quantizer.java
+++ lista5/Quantizer.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-
+import java.util.*;
 
 public class Quantizer {
   private final TGAAnalyzer tga;
@@ -25,12 +21,6 @@ public class Quantizer {
         bitmap[i][j] = codebook[Arrays.asList(diffs).indexOf(Collections.min(Arrays.asList(diffs)))];
       }
     }
-//    for (int i = 0; i < 2; i++) {
-//      for (int j = 0; j < 5; j++) {
-//        System.out.print(bitmap[i][j] + " ");
-//      }
-//      System.out.println();
-//    }
     return tga.getTGABytes(tga.getBytesFromBitmap(bitmap));
   }
   
@@ -48,16 +38,13 @@ public class Quantizer {
     ArrayList<Double[]> data = castBitmapToVectors(tga.getBitmap());
     Double[] c0 = averageVectorOfVectors(data);
     codebook.add(c0);
-    
-    
-    double averageDistortion = averageDistortionC0(c0, data, data.size());
+  
+    double averageDistortion = averageDistortion(c0, data, data.size());
     
     while (codebook.size() < codebookSize) {
       Pair<ArrayList<Double[]>, Double> result = splitCodebook(data, codebook, epsilon, averageDistortion);
       codebook = result.first;
       averageDistortion = result.second;
-//      System.out.println("averageDistortion = " + averageDistortion);
-//      System.out.println("codebook = " + codebook);
     }
     return castCodebook(codebook);
   }
@@ -92,14 +79,23 @@ public class Quantizer {
     return averageVector;
   }
   
-  private double averageDistortionC0(Double[] c0, ArrayList<Double[]> vectors, int size) {
-    ArrayList<Double> vecs = new ArrayList<>();
-    vecs.add(0.0);
+  private double averageDistortion(Double[] vector0, ArrayList<Double[]> vectors, int size) {
+    ArrayList<Double> vectorsEuclid = new ArrayList<>();
+    vectorsEuclid.add(0.0);
     for (Double[] vector :
         vectors) {
-      vecs.add(euclidSquared(c0, vector));
+      vectorsEuclid.add(euclidSquared(vector0, vector));
     }
-    return vecs.stream().reduce((s, d) -> s + d / size).get();
+    return vectorsEuclid.stream().reduce((s, d) -> s + d / size).get();
+  }
+  
+  private double averageDistortion(ArrayList<Double[]> vectorsA, ArrayList<Double[]> vectorsB, int size) {
+    ArrayList<Double> vectorsEuclid = new ArrayList<>();
+    vectorsEuclid.add(0.0);
+    for (int i = 0; i < vectorsA.size(); i++) {
+      vectorsEuclid.add(euclidSquared(vectorsA.get(i), vectorsB.get(i)));
+    }
+    return vectorsEuclid.stream().reduce((s, d) -> s + d / size).get();
   }
   
   
@@ -114,79 +110,63 @@ public class Quantizer {
   
   private Pair<ArrayList<Double[]>, Double> splitCodebook(ArrayList<Double[]> data, ArrayList<Double[]> codebook, double epsilon, double initialAvgDist) {
     int dataSize = data.size();
-    ArrayList<Double[]> new_codevectors = new ArrayList<>();
+    ArrayList<Double[]> newCodebook = new ArrayList<>();
     for (Double[] c : codebook) {
-      new_codevectors.add(new_codevector(c, epsilon));
-      new_codevectors.add(new_codevector(c, -epsilon));
+      newCodebook.add(newVector(c, epsilon));
+      newCodebook.add(newVector(c, -epsilon));
     }
-    codebook = new_codevectors;
-    System.out.println("codevectors");
-    codebook.forEach(doubles -> System.out.println(Arrays.toString(doubles)));
-    int len_codebook = codebook.size();
-    
-    double avg_dist = 0.0;
-    double err = epsilon + 1;
-    while (err > epsilon) {
-      ArrayList<Double[]> closest_c_list = new ArrayList<>(dataSize);
+    codebook = newCodebook;
+  
+    System.out.println("Splitting " + codebook.size());
+  
+    double averageDistortion = 0.0;
+    double error = epsilon + 1;
+    while (error > epsilon) {
+      ArrayList<Double[]> closest = new ArrayList<>(dataSize);
       for (int i = 0; i < dataSize; i++) {
-        closest_c_list.add(null);
+        closest.add(null);
       }
-      HashMap<Integer, ArrayList<Double[]>> vecs_near_c = new HashMap<>();
-      HashMap<Integer, ArrayList<Integer>> vecs_idxs_near_c = new HashMap<>();
+      DefaultDict<Integer, ArrayList<Double[]>> nearestVectors = new DefaultDict<>(ArrayList.class);
+      DefaultDict<Integer, ArrayList<Integer>> nearestVectorsIndexes = new DefaultDict<>(ArrayList.class);
       for (int i = 0; i < data.size(); i++) {
-        double min_dist = -1;
-        int closest_c_index = -1;
+        double minDist = -1;
+        int closestIndex = -1;
         for (int j = 0; j < codebook.size(); j++) {
           double d = euclidSquared(data.get(i), codebook.get(j));
-          if (j == 0 || d < min_dist) {
-            min_dist = d;
-            closest_c_list.set(i, codebook.get(j));
-            closest_c_index = j;
+          if (j == 0 || d < minDist) {
+            minDist = d;
+            closest.set(i, codebook.get(j));
+            closestIndex = j;
           }
         }
-        vecs_near_c.getOrDefault(closest_c_index, new ArrayList<>()).add(data.get(i));
-        vecs_idxs_near_c.getOrDefault(closest_c_index, new ArrayList<>()).add(i);
-        if (i == 0) {
-          System.out.println("vecs_idxs_near_c = " + vecs_idxs_near_c);
+        nearestVectors.get(closestIndex).add(data.get(i));
+        nearestVectorsIndexes.get(closestIndex).add(i);
+      }
+    
+      for (int i = 0; i < codebook.size(); i++) {
+        ArrayList<Double[]> nearestVectorsOfI = nearestVectors.get(i);
+        if (nearestVectorsOfI.size() > 0) {
+          Double[] averageVector = averageVectorOfVectors(nearestVectorsOfI);
+          codebook.set(i, averageVector);
+          nearestVectorsIndexes.get(i).forEach(integer -> closest.set(integer, averageVector));
         }
       }
-      System.out.println("vecs_idxs_near_c = " + vecs_idxs_near_c);
-      
-      for (int i = 0; i < len_codebook; i++) {
-        ArrayList<Double[]> vecs = vecs_near_c.getOrDefault(i, new ArrayList<>());
-        int num_vecs_near_c = vecs.size();
-        if (num_vecs_near_c > 0) {
-          Double[] new_c = averageVectorOfVectors(vecs);
-          codebook.set(i, new_c);
-          vecs_idxs_near_c.get(i).forEach(integer -> closest_c_list.set(integer, new_c));
-        }
-      }
-      
-      double prev_avg_dist = avg_dist > 0.0 ? avg_dist : initialAvgDist;
-      avg_dist = averageDistortionCList(closest_c_list, data, dataSize);
-      
-      err = (prev_avg_dist - avg_dist) / prev_avg_dist;
+    
+      double prevAvgDist = averageDistortion > 0.0 ? averageDistortion : initialAvgDist;
+      averageDistortion = averageDistortion(closest, data, dataSize);
+    
+      error = (prevAvgDist - averageDistortion) / prevAvgDist;
     }
-    return new Pair<>(codebook, avg_dist);
+    return new Pair<>(codebook, averageDistortion);
   }
   
-  private Double[] new_codevector(Double[] vector, double epsilon) {
-    Double[] newCV = new Double[3];
+  private Double[] newVector(Double[] vector, double epsilon) {
+    Double[] newVector = new Double[3];
     for (int i = 0; i < vector.length; i++) {
-      newCV[i] = vector[i] * (1.0 + epsilon);
+      newVector[i] = vector[i] * (1.0 + epsilon);
     }
-    return newCV;
+    return newVector;
   }
-  
-  private double averageDistortionCList(ArrayList<Double[]> c_list, ArrayList<Double[]> vectors, int size) {
-    ArrayList<Double> vecs = new ArrayList<>();
-    vecs.add(0.0);
-    for (int i = 0; i < c_list.size(); i++) {
-      vecs.add(euclidSquared(c_list.get(i), vectors.get(i)));
-    }
-    return vecs.stream().reduce((s, d) -> s + d / size).get();
-  }
-  
   
   private Double[] getPixelAsDoubleArray(Pixel a) {
     Double[] x = new Double[3];
@@ -196,7 +176,7 @@ public class Quantizer {
     return x;
   }
   
-  class Pair<U, V> {
+  static class Pair<U, V> {
     U first;
     V second;
     
@@ -206,5 +186,27 @@ public class Quantizer {
     }
   }
   
+  public static class DefaultDict<K, V> extends HashMap<K, V> {
+    
+    Class<V> klass;
+    
+    public DefaultDict(Class klass) {
+      this.klass = klass;
+    }
+    
+    @Override
+    public V get(Object key) {
+      V returnValue = super.get(key);
+      if (returnValue == null) {
+        try {
+          returnValue = klass.newInstance();
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+        this.put((K) key, returnValue);
+      }
+      return returnValue;
+    }
+  }
   
 }
